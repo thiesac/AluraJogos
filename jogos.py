d8cbee0a6e1622ea68740665664d356407ab87cd
thiesac <thiesa.c@gmail.com>
2022-01-11 09:29:45 -0400

    Initial commit

---
+++ jogos.py
@@ -0,0 +1,23 @@
+import forca
+import adivinhacao
+
+def escolher_jogo():
+print('*********************************')
+print('********Escolha o seu jogo*******')
+print('*********************************')
+
+print('(1) Forca   (2) Adivinhação' )
+
+jogo = int(input('Qual jogo?'))
+
+if(jogo == 1):
+    print('Jogando Forca')
+    forca.jogar()
+elif(jogo == 2):
+    print('Jogando Adivinhação')
+    adivinhacao.jogar()
+else:
+    print('Digite um número entre 1 e 2.')
+
+if(__name__ == '__main__'):
+    escolher_jogo()
